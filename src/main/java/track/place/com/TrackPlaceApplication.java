8c87faa98abbee08c484fe9b5c0e1ed45f0badb3
petros <tsatsis.pe@gmail.com>
2025-06-21 23:19:24 +0300

    DEV: First Commit

---
+++ src/main/java/track/place/com/TrackPlaceApplication.java
@@ -0,0 +1,13 @@
+package track.place.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackPlaceApplication.class, args);
+	}
+
+}
